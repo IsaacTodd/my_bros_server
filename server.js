f0094568bab2255c0cb3cee6226b1e58e478ea39
isaactodd <boom2222tv@gmail.com>
2025-11-13 22:24:56 +0700

    Add swagger docs and clean up some comments.

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 const express = require('express');
 const app = express();
+const  swaggerUi  = require( "swagger-ui-express");
+const YAML = require("yamljs");
 require('dotenv').config();
 require('./initialize_db')();
 
 
 const port = process.env.PORT ||  3000;
 
-
-
-// app.get('/', (req, res) => {
-
-//   res.send('Hello World!')
-  
-// })
-
+const swaggerDocument = YAML.load("./swagger.yaml");
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(require('./routes/user'));
 app.use("/admin", require('./routes/admin'));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })

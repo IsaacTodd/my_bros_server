14ed43b875944532ab6efc6483d08484c20b097c
IsaacTodd <boom2222tv@gmail.com>
2026-03-18 07:54:32 +0700

    Adding 'Bearer' support.

--- routes/middleware.js
+++ routes/middleware.js
@@ -1,25 +1,25 @@
 var jwt = require('jsonwebtoken');
 
-function is_admin(req,res,next) {
-    const token = req.headers['authorization'] || "";
-    if(!token) return res.status(400).json({"error":"invalidtoken"});
-    jwt.verify(token, process.env.JWT_SECRET);
-    const user_data = jwt.decode(token);
-    if(user_data?.role === "user") return res.status(401).json({"error":"unauthorized"});
-    req['x_user_data'] = user_data;
-    return next();
+function is_admin(req, res, next) {
+  const token = (req.headers['authorization'] || "").replaceAll("Bearer", "").trim();
+  if (!token) return res.status(400).json({ "error": "invalidtoken" });
+  jwt.verify(token, process.env.JWT_SECRET);
+  const user_data = jwt.decode(token);
+  if (user_data?.role === "user") return res.status(401).json({ "error": "unauthorized" });
+  req['x_user_data'] = user_data;
+  return next();
 
 }
 
-function is_user(req,res,next) {
-    const token = req.headers['authorization'] || "";
-    if(!token) return res.status(400).json({"error":"invalidtoken"});
-    jwt.verify(token, process.env.JWT_SECRET);
-    const user_data = jwt.decode(token);
-    req['x_user_data'] = user_data;
-    return next();
+function is_user(req, res, next) {
+  const token = (req.headers['authorization'] || "").replaceAll("Bearer", "").trim();
+  if (!token) return res.status(400).json({ "error": "invalidtoken" });
+  jwt.verify(token, process.env.JWT_SECRET);
+  const user_data = jwt.decode(token);
+  req['x_user_data'] = user_data;
+  return next();
 
 }
 
 
-module.exports =  {is_admin , is_user};
+module.exports = { is_admin, is_user };
